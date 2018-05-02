794a6c4c49cac24dc1397dae72b4e58ab2d966dc
flurryjung <flurryjung@gmail.com>
2018-05-01 21:56:04 -0400

    edited occlusion setting on camera

---
+++ VidGameScenes-master/NewEnvironment/Assets/MovingCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCamera : MonoBehaviour
+{
+    public Vector3 teleportPoint;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + transform.forward * Time.deltaTime);
+    }
+}
